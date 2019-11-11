f564138def487a85bc1d05657b6fab1bb1a88733
Qing <chang.qing@advantech.com.cn>
2019-11-11 10:37:35 +0800

    add msghub

---
+++ msghub/pkg/controller.go
@@ -0,0 +1,5 @@
+package msghub
+
+import (
+
+)
---
+++ msghub/pkg/msghub.go
@@ -0,0 +1,41 @@
+package msghub
+
+import (
+	"k8s.io/klog"
+	"github.com/jwzl/beehive/pkg/core"
+	"github.com/jwzl/beehive/pkg/core/context"
+)
+
+const (
+	ModuleNameHub= "edge/dgtwin"
+)
+
+type MsgHub struct {
+	context		*context.Context
+}
+
+// Register this module.
+func Register(){	
+	mh := &MsgHub{}
+	core.Register(mh)
+}
+
+//Name
+func (mh *MsgHub) Name() string {
+	return ModuleNameHub
+}
+
+//Group
+func (mh *MsgHub) Group() string {
+	return ModuleNameHub
+}
+
+//Start this module.
+func (mh *MsgHub) Start(c *context.Context) {
+	klog.Infof("Start the module!")
+}
+
+//Cleanup
+func (mh *MsgHub) Cleanup() {
+	mh.context.Cleanup(mh.Name())
+}
