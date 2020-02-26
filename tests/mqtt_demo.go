f62beb42f67830ecbf84ac435c537d60c1f7c02b
Qing <cnj1990@sina.com.cn>
2020-02-26 14:50:38 +0800

    fix the mqtt_demo

--- tests/mqtt_demo.go
+++ tests/mqtt_demo.go
@@ -12,7 +12,7 @@ import (
 	"github.com/jwzl/wssocket/fifo"
 	"github.com/jwzl/wssocket/model"
 	"github.com/jwzl/edgeOn/msghub/config"	
-	"github.com/jwzl/edgeOn/dgtwin/types"
+	"github.com/jwzl/edgeOn/common"
 )
 
 const (
@@ -183,8 +183,8 @@ func (hc * Controller) routeFromMqtt(stop chan struct{}){
 			continue
 		} 	
 
-		if msg.GetOperation() != types.DGTWINS_OPS_RESPONSE {
-			if msg.GetOperation() == types.DGTWINS_OPS_SYNC {
+		if msg.GetOperation() != common.DGTWINS_OPS_RESPONSE {
+			if msg.GetOperation() == common.DGTWINS_OPS_SYNC {
 				v, exist := hc.SyncMsgCache.Load(parentID)
 				if exist {
 					ch, isThisType := v.(chan *model.Message)
@@ -202,7 +202,7 @@ func (hc * Controller) routeFromMqtt(stop chan struct{}){
 	}
 }
 
-func (hc * Controller) WaitForMsgReply(msg *model.Message, timeout time.Duration)(*types.DGTwinResponse, error){
+func (hc * Controller) WaitForMsgReply(msg *model.Message, timeout time.Duration)(*common.TwinResponse, error){
 	timeStamp := msg.GetTimestamp()/1e3
 	msgID := msg.GetID()
 
@@ -219,7 +219,7 @@ func (hc * Controller) WaitForMsgReply(msg *model.Message, timeout time.Duration
 					return nil, errors.New("error message type")
 				}
 
-				rspMsg, err := types.UnMarshalResponseMessage(msg)
+				rspMsg, err := common.UnMarshalResponseMessage(msg)
 				if err != nil {
 					return nil, err
 				}
@@ -232,14 +232,14 @@ func (hc * Controller) WaitForMsgReply(msg *model.Message, timeout time.Duration
 }
 
 func (hc * Controller) doTwinMsgSend(from, to, operation, resource string, 
-			twins []*types.DigitalTwin, timeout time.Duration )(*types.DGTwinResponse, error) {
+			twins []common.DeviceTwin, timeout time.Duration )(*common.TwinResponse, error) {
 	//build twin message
-	msgContent, err := types.BuildTwinMessage(operation, twins)
+	msgContent, err := common.BuildTwinMessage(twins)
 	if err != nil {
 		return nil, err
 	}
 	klog.Infof("msg content (%v)", string(msgContent))	
-	modelMsg := types.BuildModelMessage(from, to, 
+	modelMsg := common.BuildModelMessage(from, to, 
 						operation, resource, msgContent)
 	
 	hc.LastSndMsgID = modelMsg.GetID()
@@ -259,123 +259,121 @@ func (hc * Controller) doTwinMsgSend(from, to, operation, resource string,
 }
 /*
 * updateDevice:
-* create/update twin or twin attributes.  
+* create twin just only need the twin ID.  
 */
-func (hc * Controller) UpdateTwins(iscloud bool, twins []*types.DigitalTwin, 
-							timeout time.Duration) (*types.DGTwinResponse, error) {
+func (hc * Controller) CreateTwins(iscloud bool, twinID string, 
+							timeout time.Duration) (*common.TwinResponse, error) {
 	var from string
  
 	if iscloud {
-		from  = types.CloudName 
+		from  = common.CloudName 
 	}else{ 
-		from  = types.EdgeAppName
+		from  = common.EdgeAppName
 	}
 
-	return hc.doTwinMsgSend(from, types.TwinModuleName, 
-				types.DGTWINS_OPS_UPDATE, types.DGTWINS_RESOURCE_TWINS, twins, timeout)		
+	twins := []common.DeviceTwin{common.DeviceTwin{ID: twinID}}	
+	return hc.doTwinMsgSend(from, common.TwinModuleName, 
+				common.DGTWINS_OPS_CREATE, common.DGTWINS_RESOURCE_TWINS, twins, timeout)		
 }
 
 /*
 * Delete twin.
 */
 func (hc * Controller) DeleteTwins(iscloud bool, twinID	string, 
-					timeout time.Duration) (*types.DGTwinResponse, error) {
+					timeout time.Duration) (*common.TwinResponse, error) {
 	var from string
-	twins := []*types.DigitalTwin{&types.DigitalTwin{
-		ID: twinID,	
-	}}
-	
  
 	if iscloud {
-		from  = types.CloudName 
+		from  = common.CloudName 
 	}else{ 
-		from  = types.EdgeAppName
+		from  = common.EdgeAppName
 	}
-
-	return hc.doTwinMsgSend(from, types.TwinModuleName, 
-				types.DGTWINS_OPS_DELETE, types.DGTWINS_RESOURCE_TWINS, twins, timeout)
+	
+	twins := []common.DeviceTwin{common.DeviceTwin{ID: twinID}}
+	return hc.doTwinMsgSend(from, common.TwinModuleName, 
+				common.DGTWINS_OPS_DELETE, common.DGTWINS_RESOURCE_TWINS, twins, timeout)
 }
 
 /*
 * Get all twin information.
 */
 func (hc * Controller) GetDevice(iscloud bool, twinID []string, 
-				timeout time.Duration) (*types.DGTwinResponse, error){
+				timeout time.Duration) (*common.TwinResponse, error){
 
-	twins := make([]*types.DigitalTwin, 0)
+	twins := make([]common.DeviceTwin, 0)
 	var from string
 
 	for _, id := range twinID {
-		twins = append(twins,  &types.DigitalTwin{
+		twins = append(twins,  common.DeviceTwin{
 			ID: id,	
 		})
 	}
  
 	if iscloud {
-		from  = types.CloudName 
+		from  = common.CloudName 
 	}else{ 
-		from  = types.EdgeAppName
+		from  = common.EdgeAppName
 	}
 
-	return hc.doTwinMsgSend(from, types.TwinModuleName, 
-				types.DGTWINS_OPS_GET, types.DGTWINS_RESOURCE_TWINS, twins, timeout)
+	return hc.doTwinMsgSend(from, common.TwinModuleName, 
+				common.DGTWINS_OPS_GET, common.DGTWINS_RESOURCE_TWINS, twins, timeout)
 }
 
 /*
 * update twin's property.
 */
-func (hc * Controller) UpdateProperty(iscloud bool, dgTwin *types.DigitalTwin, 
-							timeout time.Duration)(*types.DGTwinResponse, error){
+func (hc * Controller) UpdateProperty(iscloud bool, twin *common.DeviceTwin, 
+							timeout time.Duration)(*common.TwinResponse, error){
 
 	var from string
- 	twins := []*types.DigitalTwin{dgTwin}
+ 	twins := []common.DeviceTwin{*twin}
 	
 	if iscloud {
-		from  = types.CloudName 
+		from  = common.CloudName 
 	}else{ 
-		from  = types.EdgeAppName
+		from  = common.EdgeAppName
 	}
 
-	return hc.doTwinMsgSend(from, types.TwinModuleName, 
-				types.DGTWINS_OPS_UPDATE, types.DGTWINS_RESOURCE_PROPERTY, twins, timeout)	
+	return hc.doTwinMsgSend(from, common.TwinModuleName, 
+				common.DGTWINS_OPS_UPDATE, common.DGTWINS_RESOURCE_PROPERTY, twins, timeout)	
 }
 
 /*
 * Delete twin's property.
 */
-func (hc * Controller) DeleteProperty(iscloud bool, dgTwin *types.DigitalTwin, 
-							timeout time.Duration)(*types.DGTwinResponse, error){
+func (hc * Controller) DeleteProperty(iscloud bool, twin *common.DeviceTwin, 
+							timeout time.Duration)(*common.TwinResponse, error){
 
 	var from string
-	twins := []*types.DigitalTwin{dgTwin}
+	twins := []common.DeviceTwin{*twin}
  
 	if iscloud {
-		from  = types.CloudName 
+		from  = common.CloudName 
 	}else{ 
-		from  = types.EdgeAppName
+		from  = common.EdgeAppName
 	}
 
-	return hc.doTwinMsgSend(from, types.TwinModuleName, 
-				types.DGTWINS_OPS_DELETE, types.DGTWINS_RESOURCE_PROPERTY, twins, timeout)
+	return hc.doTwinMsgSend(from, common.TwinModuleName, 
+				common.DGTWINS_OPS_DELETE, common.DGTWINS_RESOURCE_PROPERTY, twins, timeout)
 }
 
 /*
 * Get twin's property.
 */
-func (hc * Controller) GetProperty(iscloud bool, dgTwin *types.DigitalTwin, 
-							timeout time.Duration)(*types.DGTwinResponse, error){
+func (hc * Controller) GetProperty(iscloud bool, twin *common.DeviceTwin, 
+							timeout time.Duration)(*common.TwinResponse, error){
 
 	var from string
-	twins := []*types.DigitalTwin{dgTwin}
+	twins := []common.DeviceTwin{*twin}
  
 	if iscloud {
-		from  = types.CloudName 
+		from  = common.CloudName 
 	}else{ 
-		from  = types.EdgeAppName
+		from  = common.EdgeAppName
 	}
 
-	return hc.doTwinMsgSend(from, types.TwinModuleName, 
-				types.DGTWINS_OPS_GET, types.DGTWINS_RESOURCE_PROPERTY, twins, timeout)
+	return hc.doTwinMsgSend(from, common.TwinModuleName, 
+				common.DGTWINS_OPS_GET, common.DGTWINS_RESOURCE_PROPERTY, twins, timeout)
 }
 
 /*
@@ -390,7 +388,7 @@ func (hc * Controller) GetProperty(iscloud bool, dgTwin *types.DigitalTwin,
 *	
 *	## stop the watch.
 */
-func (hc * Controller) WatchProperty(iscloud bool, twins []*types.DigitalTwin, 
+/*func (hc * Controller) WatchProperty(iscloud bool, twins []*types.DigitalTwin, 
 							timeout time.Duration)(*types.DGTwinResponse, error, string){
 	
 	var from string
@@ -420,10 +418,11 @@ func (hc * Controller) createSyncChannel(parentID string){
 
 	ch := make(chan *model.Message, 128)
 	hc.SyncMsgCache.Store(parentID, ch)
-}
+}*/
 /*
 * Get the watch's result.
 */
+/*
 func (hc * Controller) GetSyncResult(parentID string, timeout time.Duration)(*types.DGTwinResponse, error){
 	v , exist := hc.SyncMsgCache.Load(parentID)
 	if !exist {
@@ -453,162 +452,60 @@ func (hc * Controller) GetSyncResult(parentID string, timeout time.Duration)(*ty
 
 	return nil, errors.New("time out.")
 } 
+*/
 
 /*
 * TODO: stop Watch.
 */
 
 func (hc * Controller) doprocess(stop chan struct{}) error{
-	// Create device.
-	desired:= make(map[string]*types.PropertyValue)
-	reported := make(map[string]*types.PropertyValue)
-	
-	desired["temperature"] = &types.PropertyValue{
-		Value: 12.5,
-	}
-	desired["Infor"] = &types.PropertyValue{
-		Value: "hello this is temp",
-	}
-	desired["Other"] = &types.PropertyValue{
-		Value: "foo bar zon",
-	}
-	reported["d0"] =  &types.PropertyValue{
-		Value: "d0-001",
-	}
-	reported["d1"] =  &types.PropertyValue{
-		Value: "d1-001",
-	}
-	reported["d2"] =  &types.PropertyValue{
-		Value: "d2-001",
-	}
-	device0 := &types.DigitalTwin{
-		ID:	"dev001",
-		Name:	"sensor0",
-		Description: "None",
-		State: "offline",
-		Properties: &types.TwinProperties{
-			Desired: desired,
-			Reported: reported,
-		},
-	}
-
-
-	desired0:= make(map[string]*types.PropertyValue)
-	reported0 := make(map[string]*types.PropertyValue)
-	
-	desired0["gdsd"] = &types.PropertyValue{
-		Value: 12.5,
-	}
-	reported0["sds2"] =  &types.PropertyValue{
-		Value: "qing",
-	}
-
-	device1 := &types.DigitalTwin{
-		ID:	"dev002",
-		Name:	"sensor1",
-		Description: "None",
-		State: "offline",
-		Properties: &types.TwinProperties{
-			Desired: desired0,
-			Reported: reported0,
-		},
-	}
-
-	twins := []*types.DigitalTwin{device0, device1}
 
 loop:
 	//Create the device. 
-	rsp, err := hc.UpdateTwins(true, twins, 2*time.Second)
+	rsp, err := hc.CreateTwins(true, "00-0B-AB-C0-C2-C4", 2*time.Second)
 	if err != nil {
 		klog.Warningf("create device failed with err (%v)", err)
 		return err
 	}
 
-	if rsp.Code != types.RequestSuccessCode {
+	if rsp.Code != common.RequestSuccessCode {
 		klog.Warningf("create device failed with err (%d, %s)", rsp.Code, rsp.Reason)
 		return err
 	}
 
 	klog.Infof("create device Successful (%v)", rsp)
-	device0.Description = "This is Changed description ####"
-	device0.Name = "Angel boby"
-	reported["june"] =  &types.PropertyValue{
-		Value: "june is beauty",
-	}
-	//Update the device. 
-	rsp, err = hc.UpdateTwins(true, twins, 2*time.Second)
-	if err != nil {
-		klog.Warningf("Update device failed with err (%v)", err)
-		return err
-	}
-
-	if rsp.Code != types.RequestSuccessCode {
-		klog.Warningf("Update device failed with err (%d, %s)", rsp.Code, rsp.Reason)
-		return err
-	}
-	klog.Infof("update device Successful (%v)", rsp)
-
-	/* Get the device.*/
-	twinIDs := []string{"dev001", "dev002"}
+	
+	time.Sleep(3 * time.Second)
+	/* 
+	* Get the device.
+	* After create the device, edgetwin module will detect  device and match the real physical
+	* device, and then, physical device will report all information to  edgetwin module, So, we 
+	* will wait these operation is done, then getDevice will get all information about the device.   
+	*/
+	twinIDs := []string{"00-0B-AB-C0-C2-C4"}
 	rsp, err = hc.GetDevice(true, twinIDs, 2*time.Second)
 	if err != nil {
 		klog.Warningf("Get device failed with err (%v)", err)
 		return err
 	}
-	if rsp.Code != types.RequestSuccessCode {
-		klog.Warningf("Get device failed with err (%d, %s)", rsp.Code, rsp.Reason)
-		return err
-	}
+	
 	
 	klog.Infof("######## Get the result")
 	for _, dgtwin := range rsp.Twins {
 		dJSON, _ := json.Marshal(dgtwin)
 		klog.Infof("#####(%s) #######3", dJSON)
 	}
-	rsp, err = hc.DeleteTwins(true, "dev002", 2*time.Second)
+
+	rsp, err = hc.DeleteTwins(true, "00-0B-AB-C0-C2-C4", 2*time.Second)
 	if err != nil {
 		klog.Warningf("Delete device failed with err (%v)", err)
 		return err
 	}
-	if rsp.Code != types.RequestSuccessCode {
+	if rsp.Code != common.RequestSuccessCode {
 		klog.Warningf("delete device failed with err (%d, %s)", rsp.Code, rsp.Reason)
 		return err
 	}
 
-	//Create the property;
-	d := make(map[string]*types.PropertyValue)
-	r := make(map[string]*types.PropertyValue)
-
-	d["Gergia"] = &types.PropertyValue{
-		Value: "this is people.",
-	} 
-	d["Chengdu"] = &types.PropertyValue{
-		Value: "City in china.",
-	}
-
-	r["jiangjun"] =   &types.PropertyValue{
-		Value: "Nian Geng Yao.",
-	}
-	r["amber"] =   &types.PropertyValue{
-		Value: "I am a Army",
-	}
-
-	device0 = &types.DigitalTwin{
-		ID:	"dev001",
-		Properties: &types.TwinProperties{
-			Desired: d,
-			Reported: r,
-		},
-	}
-	rsp, err = hc.UpdateProperty(true, device0, 2*time.Second)
-	if err != nil {
-		klog.Warningf("Update property failed with err (%v)", err)
-		return err
-	}
-	if rsp.Code != types.RequestSuccessCode {
-		klog.Warningf("@@@@@@@@Update property failed with err (%d, %s)", rsp.Code, rsp.Reason)
-		return err
-	}
 	//update the property
 
 	// Get the property;
@@ -623,10 +520,6 @@ loop:
 	rsp, err = hc.GetDevice(true, twinIDs, 2*time.Second)
 	if err != nil {
 		klog.Warningf("Get device failed with err (%v)", err)
-		return err
-	}
-	if rsp.Code != types.NotFoundCode {
-		klog.Warningf("@@@@@@@@Get device failed with err (%d, %s)", rsp.Code, rsp.Reason)
 		return err
 	}
 	
